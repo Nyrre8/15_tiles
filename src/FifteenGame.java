8cbf0d2979fd223e51930198bfcbda2639cf76d9
Nyrre8 <144102527+Nyrre8@users.noreply.github.com>
2025-11-05 14:37:22 +0100

    Add move and switch logic

--- src/FifteenGame.java
+++ src/FifteenGame.java
@@ -1,9 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class FifteenGame extends JFrame {
+public class FifteenGame extends JFrame implements ActionListener {
     JButton[] buttonArray = new JButton[16]; // Creating an array that holds 16 JButtons
-
+    int emptyIndex = 15; // Setting emptyIndex to 15, since that is where
+    // it is when the progarm starts.
 
     public FifteenGame() {
 
@@ -21,6 +24,7 @@ public class FifteenGame extends JFrame {
 
             buttonArray[i] = new JButton(String.valueOf(i + 1));
             gridPanel.add(buttonArray[i]);
+            buttonArray[i].addActionListener(this);
 
         }
         buttonArray[15].setText(""); // Making sure the last tile for now is empty.
@@ -29,6 +33,55 @@ public class FifteenGame extends JFrame {
         add(newGameButton, BorderLayout.SOUTH);
         add(gridPanel, BorderLayout.CENTER);
         setVisible(true);
+
+    }
+
+    //Setting upp what actionPerformed should do.
+
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource(); // e.GetSource() returns object. Cast to JButton next.
+        JButton clickedButton = (JButton) source;
+
+        //Creating logic to see if it's next to the empty JButton.
+        for (int i = 0; i < buttonArray.length; i++) {
+
+            if (buttonArray[i] == clickedButton) {
+                int clickedRow = i / 4;
+                int clickedColumn = i % 4;
+
+                int emptyRow = emptyIndex / 4;
+                int emptyColumn = emptyIndex % 4;
+
+                boolean isNextToEmptyButton = false;
+
+                //Compare if ClickedRow is the same as EmptyRow
+                //If same -> make sure it differs 1 (so we know it's next to it)
+                if (clickedRow == emptyRow && Math.abs(clickedColumn - emptyColumn) == 1) {
+                    isNextToEmptyButton = true;
+                }
+
+                //Compare if clickedColumn is the same as EmptyColumn
+                //If same -> make sure it only differs 1.
+                if (clickedColumn == emptyColumn && Math.abs(clickedRow - emptyRow) == 1) {
+                    isNextToEmptyButton = true;
+                }
+
+                //If bool true, implement logic to switch text.
+                if (isNextToEmptyButton) {
+                    String textPressedButton = clickedButton.getText();
+                    String emptyText = buttonArray[emptyIndex].getText();
+
+                    clickedButton.setText(emptyText);
+                    buttonArray[emptyIndex].setText(textPressedButton);
+
+                    //Replace emptyIndex with new empty at i.
+                    emptyIndex = i;
+                }
+
+            }
+
+        }
+
 
     }
 
